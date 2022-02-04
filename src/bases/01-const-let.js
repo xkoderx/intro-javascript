198fc3a08cbd788f660c58cd289e555c1f66b8d0
Reyacido <xerickxmichelex@gmail.com>
2022-02-04 00:27:55 -0600

    01-const-let

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,13 @@
+//variableas y constantes
+/*nunca usar var*/
+const nombre = 'Erick';
+const apellico = 'Michel';
+let valordado = 5;
+valordado = 4;
+console.log(nombre,apellico,valordado);
+if (true){
+    let valordado=6;
+    const nombre = 'Pepe';
+    console.log(nombre);
+}
+console.log(valordado);
