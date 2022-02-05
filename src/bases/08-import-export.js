11adfbb3f32c951c2cbd54e7a9e214b3e210caea
Reyacido <xerickxmichelex@gmail.com>
2022-02-05 14:18:14 -0600

    08-import-export

---
+++ src/bases/08-import-export.js
@@ -0,0 +1,24 @@
+//import {heroes} from './data/heroes'
+//import { heroes } from './data/heroes';
+
+import heroes,{owners} from "./data/heroes";
+
+//console.log(heroes);
+
+// const getHeroById = (id) => {
+//     return heroes.find((heroe)=>{
+//         if (heroe.id === id){
+//             return true;
+//         }else {
+//             return false;
+//         }
+//     });
+// }
+const getHeroById = (id) => heroes.find((heroe)=> heroe.id === id);
+
+console.log(getHeroById(2));
+
+const getHeroByOwner = (owner) => heroes.filter((heroe) => heroe.owner === owner);
+
+console.log(getHeroByOwner('Marvel'));
+console.log(owners);
